d2db6b2040a404a47c7ab18575603ba516664259
Rapax <gasparpelayo44@gmail.com>
2024-09-30 21:41:15 -0300

    feat: modify submenus

--- app/(tabs)/submenus/drinks.tsx
+++ app/(tabs)/submenus/drinks.tsx
@@ -1,12 +1,73 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { ScrollView } from 'react-native';
+import { CardRow } from '@/components/CardRow';
+import { useCartContext } from '@/hooks/useCartContext';
 
-export default function MenuScreen() {
+export default function DrinksScreen() {
+  const { addToCart } = useCartContext();
+
+  const handleQuantityChange = (
+    title: string,
+    price: number,
+    quantity: number
+  ) => {
+    addToCart({ title, price, quantity });
+  };
+
   return (
-    <ThemedView className="flex-1 justify-center items-center">
-      <ThemedText type="title" className="mb-4">
-        Drinks
-      </ThemedText>
+    <ThemedView className="flex-1">
+      <ScrollView className="flex-1 p-4">
+        <ThemedText
+          type="title"
+          className="text-center text-2xl font-bold mb-4"
+        >
+          Barra
+        </ThemedText>
+        <ThemedView className="flex-row flex-wrap justify-between">
+          <CardRow
+            title="Gin Tonic"
+            price={3500}
+            icon="local-drink"
+            color="drink"
+            button={true}
+            onQuantityChange={(quantity) => {
+              handleQuantityChange('Gin Tonic', 3500, quantity);
+              console.log(`Quantity changed to ${quantity}`);
+            }}
+          />
+          <CardRow
+            title="Agua"
+            description="Agua mineral"
+            price={1200}
+            icon="water-drop"
+            color="drink"
+            button={true}
+            onQuantityChange={(quantity) =>
+              handleQuantityChange('Agua', 1200, quantity)
+            }
+          />
+          <CardRow
+            title="Cerveza"
+            price={1800}
+            color="drink"
+            button={true}
+            onQuantityChange={(quantity) =>
+              console.log(`Quantity changed to ${quantity}`)
+            }
+          />
+          <CardRow
+            title="Fernet"
+            description="Fernet, coca"
+            price={2700}
+            color="drink"
+            button={true}
+            onQuantityChange={(quantity) =>
+              console.log(`Quantity changed to ${quantity}`)
+            }
+          />
+        </ThemedView>
+      </ScrollView>
     </ThemedView>
   );
 }
--- app/(tabs)/submenus/food.tsx
+++ app/(tabs)/submenus/food.tsx
@@ -1,12 +1,65 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { ScrollView } from 'react-native';
+import { CardRow } from '@/components/CardRow';
+import { useCartContext } from '@/hooks/useCartContext';
 
-export default function MenuScreen() {
+export default function FoodScreen() {
+  const { addToCart } = useCartContext();
+
+  const handleQuantityChange = (
+    title: string,
+    price: number,
+    quantity: number
+  ) => {
+    addToCart({ title, price, quantity });
+  };
+
   return (
-    <ThemedView className="flex-1 justify-center items-center">
-      <ThemedText type="title" className="mb-4">
-        Food
-      </ThemedText>
+    <ThemedView className="flex-1">
+      <ScrollView className="flex-1 p-4">
+        <ThemedText
+          type="title"
+          className="text-center text-2xl font-bold mb-4"
+        >
+          Barra
+        </ThemedText>
+        <ThemedView className="flex-row flex-wrap justify-between">
+          <CardRow
+            title="Hamburguesa"
+            price={9500}
+            icon="fastfood"
+            color="food"
+            button={true}
+            onQuantityChange={(quantity) => {
+              handleQuantityChange('Hamburguesa', 9500, quantity);
+              console.log(`Quantity changed to ${quantity}`);
+            }}
+          />
+          <CardRow
+            title="Shawarma"
+            description="Carnivoro"
+            price={8500}
+            icon="bakery-dining"
+            color="food"
+            button={true}
+            onQuantityChange={(quantity) =>
+              handleQuantityChange('Shawarma', 8500, quantity)
+            }
+          />
+          <CardRow
+            title="Fritas"
+            description="Con sal y ketchup"
+            price={2500}
+            icon="fastfood"
+            color="food"
+            button={true}
+            onQuantityChange={(quantity) =>
+              handleQuantityChange('Fritas', 2500, quantity)
+            }
+          />
+        </ThemedView>
+      </ScrollView>
     </ThemedView>
   );
 }
